784386be443ffa3c2b5daec4038393856ffeceb2
callmeshades <47875643+callmeshades@users.noreply.github.com>
2020-08-05 19:42:46 -0600

    Final Version
    Currently the final version of the downloads cleaner. Minor changes may happen in the future.

---
+++ Downloads.cs
@@ -0,0 +1,129 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace DownloadsCleaner
+{
+    class Downloads
+    {
+        private readonly HostDetails hostDetails;
+        private List<string> folderNames;
+        private JObject jsonFiles;
+
+        public Downloads()
+        {
+            hostDetails = new HostDetails();
+            ReadJsonFile();
+            hostDetails.ChangeWorkingPath(); // Change to the downloads folder
+            CreateAllFolders();
+            MoveAllFiles();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        // Function is used for reading all information about the JSON file
+        // and storing it in memory for organizing folders
+        private void ReadJsonFile()
+        {
+            folderNames = new List<string>(); // Create the list
+
+            string filePath = "./FileTypes.json"; // JSON filepath
+
+            // Using ensures the StreamReader is automatically closed
+            // once finished reading all information
+            using (var r = new StreamReader(filePath))
+            {
+                var json = r.ReadToEnd();
+                jsonFiles = JObject.Parse(json); // Parse the Json file
+
+                // Get all names for the folders required in
+                // the Downloads folder
+                foreach (var item in jsonFiles)
+                {
+                    folderNames.Add(item.Key);
+                }
+            }
+        }
+
+        private void CreateAllFolders()
+        {
+            // Get all folders in the Downloads directory
+            // return only the names of the subfolders not the
+            // full path
+            Console.WriteLine("Search for and creating all folders!");
+            var currentDir = Directory.GetCurrentDirectory(); // To prevent calling multiple times in loop
+            foreach (var subdirectory in folderNames)
+            {
+                var di = new DirectoryInfo($@"{currentDir}\{subdirectory}");
+                // Check if the directory exists
+                // if it doesn't create it
+                if (!di.Exists)
+                {
+                    di.Create();
+                }
+            }
+        }
+
+        // Main loop for moving all files
+        // Calls other methods to complete this operation
+        private void MoveAllFiles()
+        {
+            var currentDir = Directory.GetCurrentDirectory(); // To prevent calling the directory multiple times again
+            string[] allFiles = Directory.GetFiles(currentDir);
+            foreach (var item in allFiles)
+            {
+                var currentFileType = item.Split(".");
+                CheckMatchingFiletype(item, currentFileType[currentFileType.Length - 1]);
+            }
+        }
+
+        // Used for going through all JSON objects and their key values
+        // Moves items based on children
+        private void CheckMatchingFiletype(string currentFilesPath, string currentFilesType)
+        {
+            // Go through all current parent JSON objects
+            // Parent JSON key
+            foreach (var allFileTypes in jsonFiles)
+            {
+                // Go through the current JSON objects children filenames
+                // All this information is found in FileTypes.json
+                foreach (var childrenFileType in jsonFiles[allFileTypes.Key])
+                {
+                    // If the current children key (string of text is say "png")
+                    // and equal to the current filetype for the selected file
+                    // Move that item based on the parent JSON key 
+                    if (childrenFileType.ToString().ToLower() == currentFilesType)
+                    {
+                        // Will be used by the MoveFileByItemType method
+                        MoveFileByItemType(currentFilesPath, allFileTypes.Key);
+                    }
+                }
+            }
+        }
+
+        // Actually moves the item to the respectful parent folder location
+        private void MoveFileByItemType(JToken selectedFile, string itemType)
+        {
+            // Get the current file name without the path
+            var shorthandName = Path.GetFileName(selectedFile.ToString());
+            // Builds the new filepath for the item
+            var newFilePath = @$"{hostDetails.ReturnDownloadsPath()}\{itemType}\{shorthandName}";
+
+            // Moves the file and logs it to the console
+            try
+            {
+                File.Move(selectedFile.ToString(), newFilePath);
+                Console.WriteLine($"Moved {shorthandName} to {newFilePath}");
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {shorthandName} - Reason: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ HostDetails.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DownloadsCleaner
+{
+    class HostDetails
+    {
+        private string Username { get; set; }
+        private string DownloadsPath { get; set; }
+        private string SystemDirectory { get; set; }
+
+        private void GetCurrentUsername()
+        {
+            Username = Environment.UserName;
+        }
+
+        // Fetches the current OS drive
+        // EX: The C: drive
+        private void GetOperatingSystemDrive()
+        {
+            SystemDirectory = Path.GetPathRoot(Environment.SystemDirectory);
+        }
+
+        // Builds the downloads path of the current drive and user
+        private void BuildDownloadsPath()
+        {
+            DownloadsPath = $"{SystemDirectory}Users\\{Username}\\Downloads";
+        }
+
+        // Checks to make sure the operating system is Windows
+        // Could support future OS's in the future
+        private void CheckOperatingSystem()
+        {
+            // Check if the OS is currently windows
+            // Can support other operating systems in the future
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Console.WriteLine("Currently only Windows is supported! Exiting now.");
+                Console.Write("Press any key to continue...");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+
+        // Get confirmation from the user the information about
+        // their computer is correct before moving on
+        private void ConfirmHostDetails()
+        {
+            Console.WriteLine($"Your username {Username}");
+            Console.WriteLine($"Your downloads path: {DownloadsPath}");
+
+            // Need a variable to be able to break from the while loop
+            // Improves readability
+            bool isCorrect = false;
+            while (!isCorrect)
+            {
+                Console.Write("Is this correct? (y/n): ");
+                var isCorrectKey = Console.ReadLine();
+                switch (isCorrectKey.ToLower())
+                {
+                    case "y":
+                        Console.WriteLine("Thanks for confirming! Moving on...");
+                        isCorrect = true;
+                        break;
+                    case "n":
+                        Console.WriteLine("Okay! Closing the application, please try again.");
+                        Console.WriteLine("If the problem still persists please contact support!");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid character(s) entered! Please try again.");
+                        break;
+                }
+            }
+        }
+
+        public void ChangeWorkingPath()
+        {
+            // Change the current program to be in the
+            // Downloads folder for the current user & drive
+            Directory.SetCurrentDirectory(DownloadsPath);
+        }
+
+        // Public version of the DownloadsPath
+        // Required to prevent possible altering
+        public string ReturnDownloadsPath()
+        {
+            return DownloadsPath;
+        }
+
+        public HostDetails()
+        {
+            // Grab the current host information before continuing on
+            // Will need to implement checking to confirm these details are correct
+            Console.WriteLine("Grabbing OS/host information...");
+            GetOperatingSystemDrive();
+            GetCurrentUsername();
+            BuildDownloadsPath();
+            CheckOperatingSystem();
+            /*ConfirmHostDetails();*/
+        }
+        
+    }
+}
